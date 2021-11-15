6ab265c9f937d66dfd53a6f08739ce7a1569f4ef
FabrettiMarco <mfabretti2000@gmail.com>
2021-11-15 17:06:13 +0100

    Fine

---
+++ src/org/generation/italy/security/PasswordGeneratr.java
@@ -0,0 +1,17 @@
+package org.generation.italy.security;
+
+public class PasswordGeneratr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nome="Pinco";
+		String cognome="Pallo";
+		String colore="Magenta";
+		int giorno=12;
+		int mese=05;
+		int anno=1994;
+		
+		System.out.println(nome+"-"+cognome+"-"+colore+"-"+anno);
+	}
+
+}
